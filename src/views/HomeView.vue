66bfc6002ad8ee396bd80ede3fc7b80810a85e34
phunghung0901xx <phunghung0901xx@gmail.com>
2023-05-05 16:50:11 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue'
+
+
+interface Restaurant {
+  name?:string,
+  address?:string,
+  status?:string,
+  dishes?: Dish[]
+}
+
+
+ const restaurantList = ref<Restaurant[]>([])
+ const newRestaurant = ref<Restaurant>({})
+
+ function addRestaurant() {
+   restaurantList.value.push({
+    name: newRestaurant.value.name,
+    address: '',
+    status: '',
+    dishes: []
+  })
+ }
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <pre>
+      {{ newRestaurant }}
+    </pre>
+    <form @submit.prevent="addRestaurant">
+       <div>
+          <label for="restaurant-name">Restaurant Address</label>
+      <input id="restaurant-name" v-model="newRestaurant.name" type="text"/>
+      <button type="submit">Add restaurant</button> 
+       </div>
+   <div>
+      <label for="restaurant-address">Restaurant Address</label>
+      <input id="restaurant-address" v-model="newRestaurant.address" type="text"/>
+      <button type="submit">Add restaurant</button> 
+      </div>
+    </form>
+    <ul>
+       <li v-for="restaurant in restaurantList" :key ="restaurant">
+         {{ restaurant.name }}
+      </li>
+    </ul>
   </main>
 </template>
