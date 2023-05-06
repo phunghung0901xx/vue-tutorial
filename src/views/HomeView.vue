dafe1820654d356827d5cf7464289a830f7c59a2
phunghung0901xx <phunghung0901xx@gmail.com>
2023-05-06 14:03:47 +0700

    add basic syntax

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,19 +5,33 @@ import {ref} from 'vue'
 interface Restaurant {
   name?:string,
   address?:string,
-  status?:string,
+  status?:RestaurantStatus,
   dishes?: Dish[]
 }
+
+// type RestaurantStatus  = 'Want To Try' | 'Recommended'| 'Do Not Recommended' | 'Must Try'
 
 
  const restaurantList = ref<Restaurant[]>([])
  const newRestaurant = ref<Restaurant>({})
+ 
+ const ALL_STATUS = ['Want to Try','Recommend','Must Try']
+
+ type RestaurantStatus = typeof ALL_STATUS
+
+ const statusList = [
+   'Want To Try',
+    'Recommended',
+    'Do Not Recommended',
+    'Must Try'
+
+ ]
 
  function addRestaurant() {
    restaurantList.value.push({
     name: newRestaurant.value.name,
     address: '',
-    status: '',
+    status: 'Want To Try',
     dishes: []
   })
  }
@@ -35,10 +49,17 @@ interface Restaurant {
       <input id="restaurant-name" v-model="newRestaurant.name" type="text"/>
       <button type="submit">Add restaurant</button> 
        </div>
-   <div>
+      <!-- <div>
       <label for="restaurant-address">Restaurant Address</label>
       <input id="restaurant-address" v-model="newRestaurant.address" type="text"/>
       <button type="submit">Add restaurant</button> 
+      </div> -->
+      <div>
+      <label for="restaurant-status">Restaurant Status</label>
+       <select name ="restaurant-status"
+        id="restaurant-status">
+      <option v-for="status in statusList" :value="status" :key="status">{{ status }}</option> 
+      </select>
       </div>
     </form>
     <ul>
